84d2b87a3cb98e0b946e6831c439fef9e48a6017
Johann Ramos <johannsr@hotmail.com>
2021-05-03 16:49:52 -0300

    adicionando o exercicio de tabuada

---
+++ src/Tabuada.java
@@ -0,0 +1,11 @@
+public class Tabuada {
+    public static void main(String[] args) {
+        for(int numero = 0; numero <= 10; numero++){
+            for(int contador = 0; contador <=10; contador++){
+                System.out.println(numero*contador);
+            }
+
+        }
+
+    }
+}
