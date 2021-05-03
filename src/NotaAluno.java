c99692b745ecabca6facf04963b021017ac3aa3f
Johann Ramos <johannsr@hotmail.com>
2021-05-03 18:24:32 -0300

    Exercicio Teste Nota do Aluno

--- src/NotaAluno.java
+++ src/NotaAluno.java
@@ -1,3 +1,11 @@
+/*
+Faça um método que calcule a média de um aluno de acordo com o critério definido neste
+curso. Além disso, faça um outro método que informe o status do aluno de acordo com a tabela a seguir:
+Nota acima de 6 à “Aprovado”
+Nota entre 4 e 6 à Conceito “Verificação Suplementar”
+Nota abaixo de 4 à Conceito “Reprovado”
+*/
+
 public class NotaAluno {
     public static void main(String[] args) {
         int Nota = 3;
